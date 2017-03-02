16a6d08d3f85b9a30421bd3d11a84a25e5845f3e
Han Hyuk Cho <han@Hans-MacBook-Air.local>
2017-03-02 09:46:57 -0500

    env var

--- app/env.js
+++ app/env.js
@@ -20,7 +20,7 @@ const CLIENT_ID = 'wGMKIVrPZrduAQZ5zgMib8t3PHqNA0e0';
 
 const FIRE_BASE = {
   apiKey: "AIzaSyDZGBlaWtfjB1o8SzsD-Aj0d_IVAg8Mc5Q",
-  authDomain: "sc-playroom.herokuapp.com",
+  authDomain: "sharing-playlist-application.firebaseapp.com",
   databaseURL: "https://sharing-playlist-application.firebaseio.com",
   storageBucket: "sharing-playlist-application.appspot.com",
   messagingSenderId: "751368319884"
@@ -30,4 +30,3 @@ const REDIRECT_URI = 'https://sc-playroom.herokuapp.com/callback';
 
 module.exports = { CLIENT_ID, FIRE_BASE, REDIRECT_URI }
 
-// authDomain: "sharing-playlist-application.firebaseapp.com",
