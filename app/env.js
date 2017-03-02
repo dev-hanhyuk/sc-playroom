51acc7b2e358bb4bfbaa68f1a8e65f49801fa9f3
Han Hyuk Cho <han@Hans-MacBook-Air.local>
2017-03-01 22:39:28 -0500

    fixed minor bugs

---
+++ app/env.js
@@ -0,0 +1,30 @@
+/* DEV */
+// const CLIENT_ID = 'WSa98CZfA32N1ew7v50WFDBEHT6DNEND';
+
+// const FIRE_BASE = {
+//   apiKey: "AIzaSyDZGBlaWtfjB1o8SzsD-Aj0d_IVAg8Mc5Q",
+//   authDomain: "sharing-playlist-application.firebaseapp.com",
+//   databaseURL: "https://sharing-playlist-application.firebaseio.com",
+//   storageBucket: "sharing-playlist-application.appspot.com",
+//   messagingSenderId: "751368319884"
+// }
+
+// const REDIRECT_URI = 'http://localhost:1337/callback';
+
+// module.exports = { CLIENT_ID, FIRE_BASE, REDIRECT_URI }
+
+
+/* PRODUCTION */
+const CLIENT_ID = 'wGMKIVrPZrduAQZ5zgMib8t3PHqNA0e0';
+
+const FIRE_BASE = {
+  apiKey: "AIzaSyDZGBlaWtfjB1o8SzsD-Aj0d_IVAg8Mc5Q",
+  authDomain: "sharing-playlist-application.firebaseapp.com",
+  databaseURL: "https://sharing-playlist-application.firebaseio.com",
+  storageBucket: "sharing-playlist-application.appspot.com",
+  messagingSenderId: "751368319884"
+}
+
+const REDIRECT_URI = 'https://sc-playroom.herokuapp.com/callback';
+
+module.exports = { CLIENT_ID, FIRE_BASE, REDIRECT_URI }
